659e9a8e1bbe42db818f51aeefee3433ddfbd420
Bektur Mamytov <bekturmamytov@gmail.com>
2022-11-27 16:17:29 +0900

    First commit

---
+++ Netflix Clone/Controllers/UpcomingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UpcomingViewController.swift
+//  Netflix Clone
+//
+//  Created by Bektur Mamytov on 26/11/22.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+
+}
---
+++ Netflix Clone/Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  Extensions.swift
+//  Netflix Clone
+//
+//  Created by Bektur Mamytov on 27/11/22.
+//
+
+import Foundation
+
+extension String {
+    var capitalizedSentence: String {
+        // 1
+        let firstLetter = self.prefix(1).capitalized
+        // 2
+        let remainingLetters = self.dropFirst().lowercased()
+        // 3
+        return firstLetter + remainingLetters
+    }
+}
